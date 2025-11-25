87a37978011ffb7d2fb7528b6bd54d270b0db447
junior <juniorxf@localhost.localdomain>
2025-11-25 08:18:33 -0500

    Arregle error de importaciones

--- src/flash-cards/flash-cards.module.ts
+++ src/flash-cards/flash-cards.module.ts
@@ -4,9 +4,10 @@ import { FlashCardsService } from './flash-cards.service';
 import { FlashCardsController } from './flash-cards.controller';
 import { Flashcard } from './entities/flash-card.entity';
 import { GroqModule } from 'src/groq/groq.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Flashcard]), GroqModule],
+  imports: [TypeOrmModule.forFeature([Flashcard]), GroqModule, AuthModule],
   providers: [FlashCardsService],
   controllers: [FlashCardsController],
 })
