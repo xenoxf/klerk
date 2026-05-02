c82029eeec13efde254fb393cfa57821a996681e
jesusadrian14 <jesuscasierra928@gmail.com>
2026-05-02 12:22:55 -0500

    kolol

--- src/main.ts
+++ src/main.ts
@@ -279,7 +279,7 @@ async function bootstrap() {
     next();
   });
 
-  //app.useGlobalGuards(new ApiKeyGuard());
+  app.useGlobalGuards(new ApiKeyGuard());
 
   // ============================================
   // INICIALIZACIÓN DEL SERVIDOR
