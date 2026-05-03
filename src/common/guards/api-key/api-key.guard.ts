f5c7176d9e75bf27718699454807c0840de43cdf
jesusadrian14 <jesuscasierra928@gmail.com>
2026-05-02 22:01:26 -0500

    incessdddd

--- src/common/guards/api-key/api-key.guard.ts
+++ src/common/guards/api-key/api-key.guard.ts
@@ -5,7 +5,7 @@ import {
   ForbiddenException,
 } from '@nestjs/common';
 
-const PUBLIC_PATHS = new Set(['/health', '/ping', '/auth/']);
+const PUBLIC_PATHS = new Set(['/health', '/ping', '/auth/', '/junior']);
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
